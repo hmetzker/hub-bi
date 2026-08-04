535f2e5dd6df6cfaa017d63363abbe819357ffa2
Haroldo Metzker <hmetzker@gmail.com>
2026-08-03 22:03:14 -0300

    Adiciona cartão CPEA

--- src/data/sectors.js
+++ src/data/sectors.js
@@ -1,122 +1,150 @@
 export const sectorsData = [
-    {
-      id: 'ads',
-      sigla: "ADS",
-      nome: "Administração Setorial",
-      descricao: "Setor responsável pela gestão administrativa da SMTR, incluindo recursos humanos e controle de contratos administrativos.",
-      imagem: "/images/setores/ads.png",
-      bis: [
-        {
-          id: 'ads-rh',
-          nome: "Recursos Humanos",
-          imagem: "/images/grh.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiYmZlYmNlMmMtYmZlNi00NTFhLWEwMjctOTZiMmIzMzE4MWQ3IiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        },
-        {
-          id: 'ads-contratos',
-          nome: "Contratos",
-          imagem: "/images/contratos.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiYjI4NDJkYmMtMjgyZi00ODliLTg2MjktMDg0NDAyOTg4MzhlIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        }
-      ]
-    },
-    {
-      id: 'ccco',
-      sigla: "CCCO",
-      nome: "Coordenadoria de Contratos de Concessão",
-      descricao: "Responsável pelo monitoramento e gestão dos contratos de concessão do transporte público municipal.",
-      imagem: "/images/setores/ccco.png",
-      bis: [
-        {
-          id: 'ccco-stpl',
-          nome: "STPL",
-          imagem: "/images/stpl.png",
-          url: "https://lookerstudio.google.com/reporting/c3920c69-cfd8-4b4e-a6f4-aee99ea0ff02/page/firqD"
-        },
-        {
-          id: 'ccco-indices',
-          nome: "Histórico de Índices",
-          imagem: "/images/indices.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiZmU0NzAyNzctMWEyNS00YzFiLTllMWQtOTUwMGRlMGFhMDhmIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        },
-        {
-          id: 'ccco-multa-contratual',
-          nome: "Multa Contratual",
-          imagem: "/images/multa_contratual.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiM2RjYmY2YTktODUzMC00MmQzLWFmN2UtYTE2MWVlYmFmNzczIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        },
-        {
-          id: 'ccco-multa-disciplinar',
-          nome: "Multa Disciplinar",
-          imagem: "/images/multa_disciplinar.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiYTkyMTc5MmEtMzI0Ni00ZDJlLTk0ODktZTM4NjQ2Yjc2MzFmIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        },
-        {
-          id: 'ccco-rmc-bip',
-          nome: "Histórico RMC e BIP",
-          imagem: "/images/rmc_bip.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiOGNkZjBiNjMtNjRhZC00ZDNiLTkyYjQtN2NlMGNmZTFjODAwIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        }
-      ]
-    },
-    {
-      id: 'adi',
-      sigla: "ADI",
-      nome: "Assessoria de Demandas Institucionais",
-      descricao: "Setor responsável pelo atendimento e acompanhamento das demandas institucionais da SMTR.",
-      imagem: "/images/setores/adi.png",
-      bis: [
-        {
-          id: 'adi-acompanhamento',
-          nome: "Acompanhamento e Controle",
-          imagem: "/images/ata.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiZTcwYjU0ZmEtMTBiMi00Y2EzLTkzZDctZjYyOTUzMjhlYTE2IiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9&pageName=ReportSection0c555e4164e3a67400da"
-        }
-      ]
-    },
-    {
-      id: 'corin',
-      sigla: "CORIN",
-      nome: "Comissão de Recursos de Infração",
-      descricao: "Résponsável pelos procedimentos recursais e julgamentos dos recursos administrativos referentes às sanções aplicadas por infração às normas regulamentares dos modos de transporte público.",
-      imagem: "/images/setores/corin.png",
-      bis: [
-        {
-          id: 'corin-processos',
-          nome: "Controle Processos",
-          imagem: "/images/corin.png",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiNGYxODE0NTMtMGU3YS00NWExLWFhZTktZjBiOGU4YzYzOTNiIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        }
-      ]
-    },
-    {
-      id: 'ouve',
-      sigla: "OUVE",
-      nome: "Ouvidoria Especializada",
-      descricao: "Setor responsável pelo recebimento e tratamento de manifestações dos cidadãos, atuando como instrumento de articulação e interlocução entre o cidadão e a Administração Pública.",
-      imagem: "/images/setores/ouvi.png",
-      bis: [
-        {
-          id: 'ouve-painel',
-          nome: "Painel Ouvidoria",
-          imagem: "/images/painel_ouvidoria.jpg",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiMDQ1Y2UyOWUtZjNkNC00MTNiLWExOGEtYWQwNDM2ZDY0ODczIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        }
-      ]
-    },
-    {
-      id: 'sejari',
-      sigla: "SEJARI",
-      nome: "Secretaria Executiva das Juntas Especiais de Recursos de Infrações",
-      descricao: "Órgão colegiado responsável pelo julgamento de recursos de infrações de trânsito.",
-      imagem: "/images/setores/sejari.png",
-      bis: [
-        {
-          id: 'sejari-painel',
-          nome: "Painel SEJARI",
-          imagem: "/images/painel_sejari.jpg",
-          url: "https://app.powerbi.com/view?r=eyJrIjoiZWI4NjQzMzEtZTI1My00ZTc0LWJkZDYtNzc4NjBkZjdkOTNkIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9"
-        }
-      ]
-    }
-  ];
+  {
+    id: "ads",
+    sigla: "ADS",
+    nome: "Administração Setorial",
+    descricao:
+      "Setor responsável pela gestão administrativa da SMTR, incluindo recursos humanos e controle de contratos administrativos.",
+    imagem: "/images/setores/ads.png",
+    bis: [
+      {
+        id: "ads-rh",
+        nome: "Recursos Humanos",
+        imagem: "/images/grh.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiYmZlYmNlMmMtYmZlNi00NTFhLWEwMjctOTZiMmIzMzE4MWQ3IiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+      {
+        id: "ads-contratos",
+        nome: "Contratos",
+        imagem: "/images/contratos.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiYjI4NDJkYmMtMjgyZi00ODliLTg2MjktMDg0NDAyOTg4MzhlIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+    ],
+  },
+  {
+    id: "ccco",
+    sigla: "CCCO",
+    nome: "Coordenadoria de Contratos de Concessão",
+    descricao:
+      "Responsável pelo monitoramento e gestão dos contratos de concessão do transporte público municipal.",
+    imagem: "/images/setores/ccco.png",
+    bis: [
+      {
+        id: "ccco-stpl",
+        nome: "STPL",
+        imagem: "/images/stpl.png",
+        url: "https://lookerstudio.google.com/reporting/c3920c69-cfd8-4b4e-a6f4-aee99ea0ff02/page/firqD",
+      },
+      {
+        id: "ccco-indices",
+        nome: "Histórico de Índices",
+        imagem: "/images/indices.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiZmU0NzAyNzctMWEyNS00YzFiLTllMWQtOTUwMGRlMGFhMDhmIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+      {
+        id: "ccco-multa-contratual",
+        nome: "Multa Contratual",
+        imagem: "/images/multa_contratual.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiM2RjYmY2YTktODUzMC00MmQzLWFmN2UtYTE2MWVlYmFmNzczIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+      {
+        id: "ccco-multa-disciplinar",
+        nome: "Multa Disciplinar",
+        imagem: "/images/multa_disciplinar.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiYTkyMTc5MmEtMzI0Ni00ZDJlLTk0ODktZTM4NjQ2Yjc2MzFmIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+      {
+        id: "ccco-rmc-bip",
+        nome: "Histórico RMC e BIP",
+        imagem: "/images/rmc_bip.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiOGNkZjBiNjMtNjRhZC00ZDNiLTkyYjQtN2NlMGNmZTFjODAwIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+    ],
+  },
+  {
+    id: "cpea",
+    sigla: "CPEA",
+    nome: "Coordenadoria de Planejamento Econômico e Avaliação",
+    descricao:
+      "Responsável pela análise e coordenação da dinâmica econômico-financeira dos serviços de transporte público municipal.",
+    imagem: "/images/setores/cpea.png",
+    bis: [
+      {
+        id: "cpea-indices",
+        nome: "Histórico de Índices",
+        imagem: "/images/indices.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiZmU0NzAyNzctMWEyNS00YzFiLTllMWQtOTUwMGRlMGFhMDhmIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+      {
+        id: "cpea-rmc-bip",
+        nome: "Histórico RMC e BIP",
+        imagem: "/images/rmc_bip.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiOGNkZjBiNjMtNjRhZC00ZDNiLTkyYjQtN2NlMGNmZTFjODAwIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+    ],
+  },
+  {
+    id: "adi",
+    sigla: "ADI",
+    nome: "Assessoria de Demandas Institucionais",
+    descricao:
+      "Setor responsável pelo atendimento e acompanhamento das demandas institucionais da SMTR.",
+    imagem: "/images/setores/adi.png",
+    bis: [
+      {
+        id: "adi-acompanhamento",
+        nome: "Acompanhamento e Controle",
+        imagem: "/images/ata.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiZTcwYjU0ZmEtMTBiMi00Y2EzLTkzZDctZjYyOTUzMjhlYTE2IiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9&pageName=ReportSection0c555e4164e3a67400da",
+      },
+    ],
+  },
+  {
+    id: "corin",
+    sigla: "CORIN",
+    nome: "Comissão de Recursos de Infração",
+    descricao:
+      "Résponsável pelos procedimentos recursais e julgamentos dos recursos administrativos referentes às sanções aplicadas por infração às normas regulamentares dos modos de transporte público.",
+    imagem: "/images/setores/corin.png",
+    bis: [
+      {
+        id: "corin-processos",
+        nome: "Controle Processos",
+        imagem: "/images/corin.png",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiNGYxODE0NTMtMGU3YS00NWExLWFhZTktZjBiOGU4YzYzOTNiIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+    ],
+  },
+  {
+    id: "ouve",
+    sigla: "OUVE",
+    nome: "Ouvidoria Especializada",
+    descricao:
+      "Setor responsável pelo recebimento e tratamento de manifestações dos cidadãos, atuando como instrumento de articulação e interlocução entre o cidadão e a Administração Pública.",
+    imagem: "/images/setores/ouvi.png",
+    bis: [
+      {
+        id: "ouve-painel",
+        nome: "Painel Ouvidoria",
+        imagem: "/images/painel_ouvidoria.jpg",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiMDQ1Y2UyOWUtZjNkNC00MTNiLWExOGEtYWQwNDM2ZDY0ODczIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+    ],
+  },
+  {
+    id: "sejari",
+    sigla: "SEJARI",
+    nome: "Secretaria Executiva das Juntas Especiais de Recursos de Infrações",
+    descricao:
+      "Órgão colegiado responsável pelo julgamento de recursos de infrações de trânsito.",
+    imagem: "/images/setores/sejari.png",
+    bis: [
+      {
+        id: "sejari-painel",
+        nome: "Painel SEJARI",
+        imagem: "/images/painel_sejari.jpg",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiZWI4NjQzMzEtZTI1My00ZTc0LWJkZDYtNzc4NjBkZjdkOTNkIiwidCI6IjkwNzI2YWVlLWQwMmMtNDlmZS05ODlmLTQ1ZGVmM2QwNjlkYyJ9",
+      },
+    ],
+  },
+];
